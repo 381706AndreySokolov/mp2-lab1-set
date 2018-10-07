c7c25fba232eb513c4585ca2198c3001fd0622f2
381706AndreySokolov <Sokolov11.nn@gmail.com>
2018-10-07 22:49:51 +0300

    All green with comments

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -12,13 +12,29 @@ TBitField::TBitField(int len)
 {
 	if (len < 0)
 		throw "Negative_length";
+
 	BitLen = len;
+	if (BitLen % (sizeof(TELEM) * 8) == 0)
+	{
+		MemLen = BitLen / (sizeof(TELEM) * 8);
+		pMem = new TELEM[MemLen];
+	}
+	else
+	{
+		MemLen = (BitLen / (sizeof(TELEM) * 8)) + 1;
+		pMem = new TELEM[MemLen];
+	}
+	for (int i = 0; i < MemLen; i++)
+	{
+		pMem[i] = 0;
+	}
+	/*BitLen = len;
 	MemLen = (len + 31) >> 5; 
 	TELEM* pMem = new TELEM [MemLen];
 	for (int i = 0; i < MemLen; i++)
 	{
 		pMem[i] = 0;
-	}
+	}*/
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
