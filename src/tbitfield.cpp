412024e0ef473a33c98181d960ce8a7284a8e54f
381706AndreySokolov <Sokolov11.nn@gmail.com>
2018-12-20 17:39:09 +0300

    Изменил отступы, поправил 4.2 в отчёте

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -10,181 +10,181 @@
 
 TBitField::TBitField(int len)
 {
-	if (len <= 0)
-		throw "Incorrect length";
-	bitLen = len;
+  if (len <= 0)
+    throw "Incorrect length";
+  bitLen = len;
 
-	memLen = (bitLen / (8 * sizeof(TELEM))) + 1;
-	pMem = new TELEM[memLen];
-	int n = bitLen / (sizeof(TELEM) * 8);
-	for (int i = 0; i < memLen; i++)
-		pMem[i] = 0;
+  memLen = (bitLen / (8 * sizeof(TELEM))) + 1;
+  pMem = new TELEM[memLen];
+  int n = bitLen / (sizeof(TELEM) * 8);
+  for (int i = 0; i < memLen; i++)
+    pMem[i] = 0;
 }
 // ---------------------------------------------------------------------------
 TBitField::TBitField(const TBitField &bf)
 {
-	bitLen = bf.bitLen;
-	memLen = bf.memLen;
-	pMem = new TELEM[memLen];
-	for (int i = 0; i < memLen; i++)
-		pMem[i] = bf.pMem[i];
+  bitLen = bf.bitLen;
+  memLen = bf.memLen;
+  pMem = new TELEM[memLen];
+  for (int i = 0; i < memLen; i++)
+    pMem[i] = bf.pMem[i];
 }
 // ---------------------------------------------------------------------------
 TBitField::~TBitField()
 {
-	delete[]pMem;
-	pMem = NULL;
+  delete[]pMem;
+  pMem = NULL;
 }
 // ---------------------------------------------------------------------------
 int TBitField::GetMemIndex(const int n) const
 {
-	if (n < 0 || n >= bitLen)
-		throw "Incorrect bit";
-	return (n / (sizeof(TELEM) * 8));
+  if (n < 0 || n >= bitLen)
+    throw "Incorrect bit";
+  return (n / (sizeof(TELEM) * 8));
 }
 // ---------------------------------------------------------------------------
 TELEM TBitField::GetMemMask(const int n) const
 {
-	if ((n < 0) || (n >= bitLen))
-		throw "Incorrect bit";
-	return 1 << ((n - 1) % (8 * sizeof(TELEM)));
+  if ((n < 0) || (n >= bitLen))
+    throw "Incorrect bit";
+  return 1 << ((n - 1) % (8 * sizeof(TELEM)));
 }
 // ---------------------------------------------------------------------------
 int TBitField::GetLength(void) const
 {
-	return bitLen;
+  return bitLen;
 }
 // ---------------------------------------------------------------------------
 void TBitField::SetBit(const int n)
 {
-	if ((n < 0) || (n >= bitLen))
-		throw "Incorrect bit";
-	pMem[GetMemIndex(n)] |= GetMemMask(n);
+  if ((n < 0) || (n >= bitLen))
+    throw "Incorrect bit";
+  pMem[GetMemIndex(n)] |= GetMemMask(n);
 }
 // ---------------------------------------------------------------------------
 void TBitField::ClrBit(const int n)
 {
-	if ((n < 0) || (n >= bitLen))
-		throw "Incorrect bit";
-	pMem[GetMemIndex(n)] &= ~GetMemMask(n);
+  if ((n < 0) || (n >= bitLen))
+    throw "Incorrect bit";
+  pMem[GetMemIndex(n)] &= ~GetMemMask(n);
 }
 // ---------------------------------------------------------------------------
 int TBitField::GetBit(const int n) const
 {
-	if ((n < 0) || (n >= bitLen))
-		throw "Incorrect bit";
-	return (pMem[GetMemIndex(n)] & GetMemMask(n));
+  if ((n < 0) || (n >= bitLen))
+    throw "Incorrect bit";
+  return (pMem[GetMemIndex(n)] & GetMemMask(n));
 }
 // ---------------------------------------------------------------------------
 TBitField& TBitField::operator=(const TBitField &bf)
 {
-	if (this != &bf)
-	{
-		delete[]pMem;
-		bitLen = bf.bitLen;
-		memLen = bf.memLen;
-		pMem = new TELEM[memLen];
-		for (int i = 0; i < memLen; i++)
-			pMem[i] = bf.pMem[i];
-	}
-	return *this;
+  if (this != &bf)
+  {
+    delete[]pMem;
+    bitLen = bf.bitLen;
+    memLen = bf.memLen;
+    pMem = new TELEM[memLen];
+    for (int i = 0; i < memLen; i++)
+      pMem[i] = bf.pMem[i];
+  }
+  return *this;
 }
 // ---------------------------------------------------------------------------
 int TBitField::operator==(const TBitField &bf) const
 {
-	int res = 1;
-	if (bitLen != bf.bitLen)
-		res = 0;
-	else
-		for (int i = 0; i < memLen; i++)
-			if (pMem[i] != bf.pMem[i])
-			{
-				res = 0;
-				break;
-			}
-	return res;
+  int res = 1;
+  if (bitLen != bf.bitLen)
+    res = 0;
+  else
+    for (int i = 0; i < memLen; i++)
+      if (pMem[i] != bf.pMem[i])
+      {
+        res = 0;
+        break;
+      }
+  return res;
 }
 // ---------------------------------------------------------------------------
 int TBitField::operator!=(const TBitField &bf) const
 {
-	int res = 0;
-	if (bitLen != bf.bitLen)
-		res = 1;
-	else
-		for (int i = 0; i < memLen; i++)
-			if (pMem[i] != bf.pMem[i])
-			{
-				res = 1;
-				break;
-			}
-	return res;
+  int res = 0;
+  if (bitLen != bf.bitLen)
+    res = 1;
+  else
+    for (int i = 0; i < memLen; i++)
+      if (pMem[i] != bf.pMem[i])
+      {
+        res = 1;
+        break;
+      }
+  return res;
 }
 // ---------------------------------------------------------------------------
 TBitField TBitField::operator|(const TBitField &bf)
 {
-	int i, len = bitLen;
-	if (bf.bitLen > len)
-		len = bf.bitLen;
-	TBitField temp(len);
-	for (i = 0; i < memLen; i++)
-		temp.pMem[i] = pMem[i];
-	for (i = 0; i < bf.memLen; i++)
-		temp.pMem[i] |= bf.pMem[i];
-	return temp;
+  int i, len = bitLen;
+  if (bf.bitLen > len)
+    len = bf.bitLen;
+  TBitField temp(len);
+  for (i = 0; i < memLen; i++)
+    temp.pMem[i] = pMem[i];
+  for (i = 0; i < bf.memLen; i++)
+    temp.pMem[i] |= bf.pMem[i];
+  return temp;
 }
 // ---------------------------------------------------------------------------
 TBitField TBitField::operator&(const TBitField &bf)
 {
-	int i, len = bitLen;
-	if (bf.bitLen > len)
-		len = bf.bitLen;
-	TBitField temp(len);
-	for (i = 0; i < memLen; i++)
-		temp.pMem[i] = pMem[i];
-	for (i = 0; i < bf.memLen; i++)
-		temp.pMem[i] &= bf.pMem[i];
-	return temp;
+  int i, len = bitLen;
+  if (bf.bitLen > len)
+    len = bf.bitLen;
+  TBitField temp(len);
+  for (i = 0; i < memLen; i++)
+    temp.pMem[i] = pMem[i];
+  for (i = 0; i < bf.memLen; i++)
+    temp.pMem[i] &= bf.pMem[i];
+  return temp;
 }
 // ---------------------------------------------------------------------------
 TBitField TBitField::operator~(void)
 {
-	TBitField temp = *this;
-	for (int i = 0; i < temp.bitLen; i++)
-	{
-		if (temp.GetBit(i))
-			temp.ClrBit(i);
-		else
-			temp.SetBit(i);
-	}
-	return temp;
+  TBitField temp = *this;
+  for (int i = 0; i < temp.bitLen; i++)
+  {
+    if (temp.GetBit(i))
+      temp.ClrBit(i);
+    else
+      temp.SetBit(i);
+  }
+  return temp;
 }
 // ---------------------------------------------------------------------------
 istream &operator>>(istream &istr, TBitField &bf)
 {
-	string temp;
-	istr >> temp;
-	if (temp.size() != bf.GetLength())
-		throw "Incorrect length";
-	for (int i = 0; i < bf.bitLen; i++)
-	{
-		if (temp[i] == '0')
-			bf.ClrBit(i);
-		else if (temp[i] == '1')
-			bf.SetBit(i);
-		else
-			throw "Incorrect input";
-	}
-	return istr;
+  string temp;
+  istr >> temp;
+  if (temp.size() != bf.GetLength())
+    throw "Incorrect length";
+  for (int i = 0; i < bf.bitLen; i++)
+  {
+    if (temp[i] == '0')
+      bf.ClrBit(i);
+    else if (temp[i] == '1')
+      bf.SetBit(i);
+    else
+      throw "Incorrect input";
+  }
+  return istr;
 }
 // ---------------------------------------------------------------------------
 ostream &operator<<(ostream &ostr, const TBitField &bf)
 {
-	for (int i = 0; i < bf.bitLen; i++)
-	{
-		if (bf.GetBit(i))
-			ostr << 1;
-		else
-			ostr << 0;
-	}
-	return ostr;
+  for (int i = 0; i < bf.bitLen; i++)
+  {
+    if (bf.GetBit(i))
+      ostr << 1;
+    else
+      ostr << 0;
+  }
+  return ostr;
 }
--- src/tset.cpp
+++ src/tset.cpp
@@ -9,133 +9,133 @@
 
 TSet::TSet(int mp) : bitField(mp)
 {
-	maxPower = mp;
+  maxPower = mp;
 }
 // ---------------------------------------------------------------------------
 TSet::TSet(const TSet &s) : bitField(s.bitField)
 {
-	maxPower = s.maxPower;
+  maxPower = s.maxPower;
 }
 // ---------------------------------------------------------------------------
 TSet::TSet(const TBitField &bf) : bitField(bf)
 {
-	maxPower = bitField.GetLength();
+  maxPower = bitField.GetLength();
 }
 // ---------------------------------------------------------------------------
 TSet::operator TBitField()
 {
-	return bitField;
+  return bitField;
 }
 // ---------------------------------------------------------------------------
 int TSet::GetMaxPower(void) const
 {
-	return maxPower;
+  return maxPower;
 }
 // ---------------------------------------------------------------------------
 int TSet::IsMember(const int Elem) const
 {
-	if (Elem < 0 || Elem >= maxPower)
-		throw "Incorrect element";
-	return bitField.GetBit(Elem);
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  return bitField.GetBit(Elem);
 }
 // ---------------------------------------------------------------------------
 void TSet::InsElem(const int Elem)
 {
-	if (Elem < 0 || Elem >= maxPower)
-		throw "Incorrect element";
-	bitField.SetBit(Elem);
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.SetBit(Elem);
 }
 // ---------------------------------------------------------------------------
 void TSet::DelElem(const int Elem)
 {
-	if (Elem < 0 || Elem >= maxPower)
-		throw "Incorrect element";
-	bitField.ClrBit(Elem);
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.ClrBit(Elem);
 }
 // ---------------------------------------------------------------------------
 TSet& TSet::operator=(const TSet &s)
 {
-	maxPower = s.maxPower;
-	bitField = s.bitField;
-	return *this;
+  maxPower = s.maxPower;
+  bitField = s.bitField;
+  return *this;
 }
 // ---------------------------------------------------------------------------
 int TSet::operator==(const TSet &s) const
 {
-	if (maxPower != s.maxPower)
-		return 0;
-	else
-		if (bitField != s.bitField)
-			return 0;
-	return 1;
+  if (maxPower != s.maxPower)
+    return 0;
+  else
+    if (bitField != s.bitField)
+      return 0;
+  return 1;
 }
 // ---------------------------------------------------------------------------
 int TSet::operator!=(const TSet &s) const
 {
-	if (maxPower != s.maxPower)
-		return 1;
-	else
-		if (bitField != s.bitField)
-			return 1;
-	return 0;
+  if (maxPower != s.maxPower)
+    return 1;
+  else
+    if (bitField != s.bitField)
+      return 1;
+  return 0;
 }
 // ---------------------------------------------------------------------------
 TSet TSet::operator+(const TSet &s)
 {
-	int len = maxPower;
-	if (s.maxPower > maxPower)
-		len = s.maxPower;
-	TSet temp(len);
-	temp.bitField = bitField | s.bitField;
-	return temp;
+  int len = maxPower;
+  if (s.maxPower > maxPower)
+    len = s.maxPower;
+  TSet temp(len);
+  temp.bitField = bitField | s.bitField;
+  return temp;
 }
 // ---------------------------------------------------------------------------
 TSet TSet::operator+(const int Elem)
 {
-	if (Elem < 0 || Elem >= maxPower)
-		throw "Incorrect element";
-	bitField.SetBit(Elem);
-	return *this;
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.SetBit(Elem);
+  return *this;
 }
 // ---------------------------------------------------------------------------
 TSet TSet::operator-(const int Elem)
 {
-	if (Elem < 0 || Elem >= maxPower)
-		throw "Incorrect element";
-	bitField.ClrBit(Elem);
-	return *this;
+  if (Elem < 0 || Elem >= maxPower)
+    throw "Incorrect element";
+  bitField.ClrBit(Elem);
+  return *this;
 }
 // ---------------------------------------------------------------------------
 TSet TSet::operator*(const TSet &s)
 {
-	int len = maxPower;
-	if (s.maxPower > maxPower)
-		len = s.maxPower;
-	TSet temp(len);
-	temp.bitField = bitField & s.bitField;
-	return temp;
+  int len = maxPower;
+  if (s.maxPower > maxPower)
+    len = s.maxPower;
+  TSet temp(len);
+  temp.bitField = bitField & s.bitField;
+  return temp;
 }
 // ---------------------------------------------------------------------------
 TSet TSet::operator~(void)
 {
-	TSet temp = *this;
-	temp.bitField = ~temp.bitField;
-	return temp;
+  TSet temp = *this;
+  temp.bitField = ~temp.bitField;
+  return temp;
 }
 // ---------------------------------------------------------------------------
 istream &operator>>(istream &istr, TSet &s)
 {
-	int i;
-	cin >> i;
-	for (; ((i > -1) && (i < s.maxPower)); cin >> i)
-		s.bitField.SetBit(i);
-	return istr;
+  int i;
+  cin >> i;
+  for (; ((i > -1) && (i < s.maxPower)); cin >> i)
+    s.bitField.SetBit(i);
+  return istr;
 }
 // ---------------------------------------------------------------------------
 ostream& operator<<(ostream &ostr, const TSet &s)
 {
-	ostr << s.bitField << ' ';
-	return ostr;
+  ostr << s.bitField << ' ';
+  return ostr;
 }
 
 
